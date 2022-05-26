ed4a2cd17d395756bda95ea45060e58158be6d93
jhansi <jhansi12mar2000@gmail.com>
2022-05-26 21:33:50 +0530

    Added my project

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React ,{useContext} from 'react'
+import { home } from './Components/ApiHome';
 
-function Home() {
+
+const Home = () => {
+  const [dataHome, setDataHome] = useContext(home);
   return (
+
     <div>
-     <h1 className='Home'>Home page </h1>
+      
+
+      <div className="contentPage">
+      
+      {dataHome.map((item) => {
+        return (
+        <a href={item.url}>
+            <div className="card">
+              <img src={item.urlToImage} alt="PopularImg"  width='320px'height='200px'/>
+              <p>{item.title}</p>
+              <p>{item.description}</p>
+              <p>{item.author}{item.publishedAt}</p>
+              <hr />
+            </div></a>
+        );
+      })}
       </div>
+      <div className="right">
+        
+      </div>
+
+    </div>
   )
 }
 
